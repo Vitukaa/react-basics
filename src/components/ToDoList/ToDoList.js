0575bd3a189e3e4b64dd2b1d99f7e58f05ec9741
Vitukaa <vitalija.dau@gmail.com>
2023-03-03 14:15:53 +0200

    Task description textarea added

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -3,11 +3,18 @@ import './ToDoList.css'
 
 const ToDoList = () => {
     const [input, setInput] = useState('')
+    const [descriptionInput, setDesciptionInput] = useState('')
     const [list, setList] = useState([])
 
     const inputHandler = (event) => {
         setInput(event.target.value)
     }
+
+
+    const descriptionInputHandler = (event) => {
+        setDesciptionInput(event.target.value)
+    }
+
 
     const submitHandler = (event) => {
         event.preventDefault()
@@ -15,11 +22,13 @@ const ToDoList = () => {
         const newTaskItem = {
             id: Math.random(),
             title: input,
+            description: descriptionInput,
             done: false,
         }
 
         setList((prevState) => [...prevState, newTaskItem])
         setInput('')
+        setDesciptionInput('')
     }
 
     const deleteHandler = (id) => {
@@ -45,27 +54,41 @@ const ToDoList = () => {
 
     }
 
-
-
-
   return (
     <>
-    <form onSubmit={submitHandler}>
-        <label htmlFor='task'>Task:</label>
-        <input type='text' name='task' value={input} onChange={inputHandler}></input>
-        <input type='submit' value='Add to list' ></input>
-    </form>
-    <h2>To Do list:</h2>
-    <ul>
-        {(list && list.length > 0) &&
-            list.map((item, index) => 
-            <li key={index} onClick={() => isDoneHandler(item.id)}>
-                <span className={item.done ? 'task-done' : 'task'}>{item.title}</span>
-                <button onClick={() => deleteHandler(item.id)}>X</button>
-            </li>)
-        }
-    </ul>
-    
+        <form onSubmit={submitHandler}>
+            <div className='form-control'>
+                <label htmlFor='task'>Task:</label>
+                <input type='text' name='task' value={input} onChange={inputHandler}></input>
+            </div>
+            <div className='form-control'>
+                <label htmlFor='task-description'>Task description:</label>
+                <textarea name='task-description' value={descriptionInput} onChange={descriptionInputHandler}></textarea>
+            </div>
+            
+            <input type='submit' value='Add to list' disabled={!input || !descriptionInput}></input>
+        </form>
+
+        <div className='tasks-wrapper'>
+            {list && list.length > 0 ? (
+                <>
+                    <h2>To Do list:</h2>
+                    <ul>
+                        {list.map((item, index) => 
+                            <li key={index} onClick={() => isDoneHandler(item.id)}>
+                                <div className='task-wrapper'>
+                                    <h4 className={item.done ? 'task-done' : 'task'}>{item.title}</h4>
+                                    {!item.done && <p>Desciption: {item.description}</p>}
+                                    <button onClick={() => deleteHandler(item.id)}>X</button>
+                                </div>
+                            </li>
+                        )}
+                    </ul>
+                </>
+            ) : (
+                <h2>No tasks added.</h2>
+            )}
+        </div>
     </>
   )
 }
